4c240f67b9ffd906cb842a19acaf342fa90a5603
JER-OFC <138082354+JER-OFC@users.noreply.github.com>
2023-12-11 23:19:22 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,11 +1,11 @@
 const fs = require('fs')
 
-global.creator = 'Zeeoneofc'// yourname
+global.creator = 'JER OFC'// yourname
 global.MONGO_DB_URI = "" //database mongodb 
-global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "" //email
-global.email_password = "" //application password email
-global.limitCount = 10000
+global.ACTIVATION_TOKEN_SECRET = "JER" //isi apa aja bebas
+global.your_email = "jeremikurniawan1000@gmail.com" //email
+global.email_password = "jeremi130410" //application password email
+global.limitCount = 10
 global.YUOR_PORT = 8000
 global.loghandler = {
 	noapikey:{
